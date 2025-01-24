2574331b91556258423ac78524691e66fa6c7bf7
Nandu-m360 <mendenandu522@gmail.com>
2025-01-24 14:10:26 +0530

    Prime and pattern

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int r=1;r<=5;r++) {// r--1
+			for(int c=5;c>=r;c--) {// 5 4 3 2 1
+				System.out.print(c+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+	}
+}
+
